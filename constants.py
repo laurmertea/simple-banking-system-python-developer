61ac06e64cd6b431f645a052759d102b99bc1723
laurmertea <mertealaur@yahoo.com>
2020-10-11 06:02:06 +0200

    Add menu, login / logout & card ops sections

---
+++ constants.py
@@ -0,0 +1,16 @@
+# MENU SECTION
+MENU_UNSUPPORTED_OPTION_MSG = 'Sorry, that option is unsupported!'
+MENU_EXIT_MSG = 'Bye!'
+
+# LOGIN / LOGOUT SECTION
+LOGIN_CARD_INPUT = 'Enter your card number:\n'
+LOGIN_PIN_INPUT = 'Enter your PIN:\n'
+LOGIN_SUCCES_MSG = '\nYou have successfully logged in!\n'
+LOGIN_FAIL_MSG = '\nWrong card number or PIN!\n'
+LOGOUT_SUCCES_MSG = '\nYou have successfully logged out!\n'
+
+# CARD OPERATIONS SECTION
+CREATE_CARD_MSG = '\nYour card has been created'
+CARD_NO_MSG = 'Your card number:'
+CARD_PIN_MSG = 'Your card PIN:'
+CARD_BALANCE_MSG = '\nBalance: '
