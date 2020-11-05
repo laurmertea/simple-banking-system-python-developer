2d81896aea3ae6386c4deb1e052f6bf8c1e4ae5b
laurmertea <mertealaur@yahoo.com>
2020-11-05 03:44:14 +0100

    Fix spacing (weak warnings)

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import random
 import constants
 from classes.card import Card
 
+
 guest_options = ['1. Create an account', '2. Log into account', '0. Exit']
 logged_in_options = ['1. Balance', '2. Log out', '0. Exit']
 logged_in = -1
 selected_option = None
 cards = []
+
 
 def login(cards):
     """Try and login with provided card data by searching card number and pin in cards array.
@@ -26,6 +28,7 @@ def login(cards):
             if card_pin == card.pin:
                 return index
     return -1
+
 
 def guest_menu(selected_option, index):
     """Try and access selected option in the guest menu (see guest_options list).
@@ -47,12 +50,13 @@ def guest_menu(selected_option, index):
         elif selected_option == 2:
             index = login(cards)
             if index != -1:
-                print(constants.LOGIN_SUCCES_MSG)
+                print(constants.LOGIN_SUCCESS_MSG)
             else:
                 print(constants.LOGIN_FAIL_MSG)
         else:
             print(constants.MENU_UNSUPPORTED_OPTION_MSG)
     return index
+
 
 def logged_in_menu(selected_option, index):
     """Try and access selected option in the logged in menu (see logegd_in_options list).
@@ -70,11 +74,12 @@ def logged_in_menu(selected_option, index):
         if selected_option == 1:
             cards[index].get_balance()
         elif selected_option == 2:
-            print(constants.LOGOUT_SUCCES_MSG)
+            print(constants.LOGOUT_SUCCESS_MSG)
             index = -1
         else:
             print(constants.MENU_UNSUPPORTED_OPTION_MSG)
     return index
+
 
 def exit_sbs(message=constants.MENU_EXIT_MSG):
     """Exit with message.
@@ -83,6 +88,7 @@ def exit_sbs(message=constants.MENU_EXIT_MSG):
         message -- string to exit with as message (default MENU_EXIT_MSG)
     """
     sys.exit(message)
+
 
 while selected_option not in range(0, 3):
     while selected_option != 0:        
