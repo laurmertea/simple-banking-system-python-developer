2f5a16155047acd07ded8b60ead94c4c4c293dbe
laurmertea <mertealaur@yahoo.com>
2020-11-07 05:54:24 +0100

    Add card operations constants

--- constants.py
+++ constants.py
@@ -14,6 +14,21 @@ CREATE_CARD_MSG = '\nYour card has been created'
 CARD_NO_MSG = 'Your card number:'
 CARD_PIN_MSG = 'Your card PIN:'
 CARD_BALANCE_MSG = '\nBalance: '
+CARD_ADD_INCOME_MSG = '\nEnter income:\n'
+CARD_ADD_INCOME_SUCCESS = '\nIncome was added!\n'
+CARD_ADD_INCOME_FAIL = '\nCould not add the given income. Please try again!\n'
+CARD_TRANSFER_MSG = '\nTransfer\n'
+CARD_TRANSFER_NUMBER_MSG = '\nEnter card number:\n'
+CARD_TRANSFER_NUMBER_FAIL = '\nProbably you made a mistake in the card number. Please try again!\n'
+CARD_TRANSFER_NUMBER_OWN = '\nYou can\'t transfer money to the same account!\n'
+CARD_TRANSFER_NUMBER_NONEXISTENT = '\nSuch a card does not exist.\n'
+CARD_TRANSFER_AMOUNT_MSG = '\nEnter how much money you want to transfer:\n'
+CARD_TRANSFER_AMOUNT_FAIL = '\nNot enough money!\n'
+CARD_TRANSFER_AMOUNT_SUCCESS = '\nSuccess!\n'
+CARD_CLOSE_MSG = '\nThe account has been closed!\n'
+
+# GENERIC
+POSITIVE_INTEGER_FAIL = '\nThe value is not a positive integer. Please try again!\n'
 
 # DATABASE SECTION
 DATABASE_FILE = 'card.s3db'
